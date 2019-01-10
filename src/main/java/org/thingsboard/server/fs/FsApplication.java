4cbfcf90c1f750ac193ec7a649b8a0396ee3d9af
liuyibin <liuyibin23@163.com>
2019-01-10 10:36:47 +0800

    '添加日志定制'

--- src/main/java/org/thingsboard/server/fs/FsApplication.java
+++ src/main/java/org/thingsboard/server/fs/FsApplication.java
@@ -1,15 +1,18 @@
 package org.thingsboard.server.fs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
+@Slf4j
 public class FsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FsApplication.class, args);
+        log.info("服务启动.....");
     }
 
 }
